1a4d24c36a57f25e336e925fe14f1a7d947bcc91
ColeVFrank <cole.v.frank@gmail.com>
2021-03-24 21:49:03 -0400

    key retrieval functions working

--- util/get_keys.py
+++ util/get_keys.py
@@ -1,13 +1,23 @@
 import os
-from core.util import basic_io
+from util import basic_io
 
 
 
-def get_twitter_key():
+def get_api_key():
     """
     get twitter api key as string
     :return: (str) key
     """
     path_to_keys = os.path.join('config', 'twitter_keys.json')
     keys = basic_io.read_json_to_dict(path_to_keys)
-    return keys["api-key"]
+    return keys["api_key"]
+
+
+def get_bearer_token():
+    """
+    get twitter bearer token as a string
+    :return: (str) key
+    """
+    path_to_keys = os.path.join('config', 'twitter_keys.json')
+    keys = basic_io.read_json_to_dict(path_to_keys)
+    return keys["bearer_token"]
